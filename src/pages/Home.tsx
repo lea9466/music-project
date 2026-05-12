8731622303918dc51180db00069222a2ac1834ce
Lea <lea0556769466@gmail.com>
2026-05-12 20:02:43 +0300

    שוב

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,40 +14,23 @@ function Home() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const newSongs = useSelector((state: RootState) => state.songs.newSongs);
-    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
-        setMounted(true);
         const loadData = async () => {
-            if (newSongs.length == 0) {
-                try {
-                    const newSongs = await getNewSongs();
-                    dispatch(setSongs({ items: newSongs }));
-                } catch (err) {
-                    console.error("שגיאה בקריאת הנתונים:", err);
-                }
+            try {
+                const fetchedSongs = await getNewSongs(); // ← שם שונה
+                dispatch(setSongs({ items: fetchedSongs }));
+            } catch (err) {
+                console.error("שגיאה בקריאת הנתונים:", err);
             }
-
         };
         loadData();
     }, []);
 
-    if (!mounted) return null;
-
-    function setShowSplash(arg0: boolean): void {
-        throw new Error('Function not implemented.');
-    }
-
     return (
-        <div className='home' suppressHydrationWarning>
-            {/* <div className="welcome" suppressHydrationWarning> */}
-            {/* <h1>Harmonia</h1>
-                <h2>כאן תוכלו למצוא אקורדים למגוון שירים ברמות שונות ובצורה הנוחה ביותר</h2>
-                <button className="menuBtns" onClick={() => navigate('categories')}>התחילו לנגן ←</button> */}
-            {/* <SplashScreen onDone={() => setShowSplash(true)} /> */}
+        <div className='home'>
             <HeroSection />
-            {/* </div> */}
-            <div className="content" suppressHydrationWarning>
+            <div className="content">
                 <h1>אקורדים חדשים באתר</h1>
                 <ChordsDisplay songs={newSongs} />
                 <h1 id="newReq">בקשות שירים מהקהילה</h1>
